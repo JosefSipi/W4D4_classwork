7acba98e36bc26f58122093fa79419f97efaee10
Joseph Sipiorski <josephsipiorski@gmail.com>
2021-03-18 14:07:07 -0500

    added two sum method and spec

--- lib/Problems.rb
+++ lib/Problems.rb
@@ -10,4 +10,16 @@ class Array
         arr
     end
 
+    def two_sum
+        pairs = []
+        self.each_with_index do |ele1, idx1|
+            self.each_with_index do |ele2, idx2|
+                if idx2 > idx1 && ele1 + ele2 == 0
+                    pairs << [idx1, idx2]
+                end
+            end
+        end
+        pairs
+    end
+    
 end
--- spec/problems_spec.rb
+++ spec/problems_spec.rb
@@ -10,6 +10,7 @@ describe Array do
 
     # subject(:arr) { [1,2,2] }
     let(:arr) { [2,3,1,2,1] }
+    let(:arr_2) {[-1, 0, 2, -2, 1 ]}
 
     describe '#my_uniq' do
 
@@ -27,7 +28,21 @@ describe Array do
 
     end
 
+    describe "#two_sum" do
+    # the indecies are sorted smaller to bigger
+    # returns an empty array if no pair is found
+    it "returns position of the pairs that add to zero" do
+        expect(arr_2.two_sum).to eq([[0,4],[2,3]])
+    end
 
+    it "Finds correct numbers of pairs" do
+        expect(arr_2.two_sum.length).to eq(2)
+        expect(arr.two_sum.length).to eq(0)
+    end
 
+    it "returns an empty array if no pair is found" do 
+        expect(arr.two_sum).to eq([])
+    end
 
+    end
 end
